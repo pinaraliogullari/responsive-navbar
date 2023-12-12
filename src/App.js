022f67cc855ce4c13eae5cdb4875aa0ca5807383
Pınar ALİOĞULLARI <aliogullarpnar@gmail.com>
2023-12-12 13:52:46 +0300

    responsive navbar with tailwind and react  is ok

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Contact from './Components/Contact';
+import Navbar from './Components/Navbar';
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Contact />
+    </>
+  );
+}
+
+export default App;
